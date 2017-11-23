7f9fcd05b437ae157f0022da2dd8c0ea753f83c9
Hbentzur <hdr.bentzur@gmail.com>
2017-11-23 13:30:20 -0500

    23_11_1330

--- sketch.js
+++ sketch.js
@@ -1,227 +1,110 @@
-var skier;
-var monster;
-var crowd = [];
-var tree = [];
-var playMode = true;
+var capture;
+var w = 1280,
+    h = 960;
 
-function preload() {
-  myFont = loadFont('BIG JOHN.otf');
-}
+var thresh = 10;
 
-function setup(){
-    createCanvas (windowWidth, windowHeight);
-    
-    skier = new SkiMan();
-    monster = new Monster();
-    
-    //tree
-    for (var i = 0; i < 6; i++){
-        tree[i] = new GreenTree(random(100, 255), random(15, 30));
-    }
-        
-    //crowd
-    for (var i = 0; i < 10; i++){
-        crowd[i] = new OtherSkiers(4, random(10, 12));
-    }
-    
-}
+let walker;
 
-function draw(){
-    background(255,255,255,160);
+function setup() {
+    capture = createCapture(VIDEO);
+    createCanvas(w, h);
+    capture.size(w, h);
+    capture.hide();
+  	walker = new Walker();
     
-    
-    //skier
-    if(playMode){
-    skier.show();
-    skier.move();
-    skier.nowayup();
-    
-    //tree
-    if (frameCount > 50){
-    for (let i = 0; i < tree.length; i++){
-        tree[i].move();
-        tree[i].show();}
-    }
-    
-    //monster
-    monster.move();
-    monster.show(); 
-    monster.eat();
-        
-    //crowd
-   if (frameCount > 10){
-    for (let i = 0; i < crowd.length; i++){
-        crowd[i].move();
-        crowd[i].show();}
-    }
-        
-    }else{
-        //fail mode
-    }
-    
-}
+createSprite(400, 200, 50, 50);
 
-class Monster {
-     constructor(skierx, skiery){
-        this.monsterx = 0;
-        this.monstery = 0;
-    } 
-    
-    move(){
-     
-        if (skier.y > 100){
-            this.monsterx = skier.x + random (sin(8,30));
-            this.monstery = frameCount % skier.y + random (sin(8,30));
-        }
-
-        
-    }
-    
-    show(){
-        textSize(25);
-        textStyle(BOLD);
-        textFont(myFont);
-        fill (100,100,100);
-        text ("MONSTER", this.monsterx, this.monstery);
-    
-}
-    
-    eat(){
-        
-        if (dist(this.monsterx,this.monstery,skier.x,skier.y) < 10){
-            SkierFail = true;
-        }
-    }
-    
-}
-
-class GreenTree {
-     constructor(Cr, Sz){
-        this.x = random(100, windowWidth - 100);
-        this.y = windowHeight + (random (10, 500));
-        this.Cr = Cr;
-        this.size = Sz;
-    }  
-    
-    move(){
-        this.x = this.x;
-        this.y = this.y - 1;
-        
-        if (keyIsDown(RIGHT_ARROW)){
-        this.y += 0.2;
-        this.x -= 1;
-        }
-        
-        if (keyIsDown(LEFT_ARROW)){
-        this.y += 0.2;
-        this.x += 1;
-        }
-        
-        // Plant new trees
-        if (this.y < 0){
-            this.y = windowHeight;
-            this.x = random(100, windowWidth - 100);
-        }
-    }
-    
-    show(){
-        textSize(this.size);
-        textStyle(BOLD);
-        textFont(myFont);
-        fill (0,this.Cr,30,100);
-        text ("TREE", this.x, this.y);
-        
-    }
-}
-
-class SkiMan {
-    constructor(){
-        this.x = 100;
-        this.y = 0;
-    }
-    
-    
-    show(){
-        textSize(10);
-        textStyle(BOLD);
-        textFont(myFont);
-        fill (255,0,0);
-        text ("SKIMAN",constrain (this.x, 0, windowWidth - 50), this.y);
-        this.y += 1;
-    }
-    
-    move(){
-        if (keyIsDown(LEFT_ARROW)){
-        this.x -= random(6,10);
-        this.y += -1;
-    }
-        
-        if (keyIsDown(RIGHT_ARROW)){
-        this.x += random(6,10);
-        this.y += -1;
-        }
-        
-    }
-    
-    nowayup(){
-        push();
-        if (keyIsDown(UP_ARROW)){
-        textSize (10);
-        textStyle(ITALIC);
-        fill (0);
-        text("DOWN THE HILL!", this.x, this.y - 30);
-        }
-        pop();
-    }
-    
-   
-}
-
-class OtherSkiers {
-    constructor(speed, size){
-        this.x = random(100, windowWidth - 100);
-        this.y = windowHeight + (random (10, 500));
-        this.speed = speed;
-        this.size = size;
-    }  
-    
-     move(){
-        this.x += random (-3,3);
-        this.y = this.y - this.speed;
-        
-        if (keyIsDown(RIGHT_ARROW)){
-        this.y += 0.2;
-        this.x -= 1;
-        }
-        
-        if (keyIsDown(LEFT_ARROW)){
-        this.y += 0.2;
-        this.x += 1;
-        }
-        
-        // new crowd
-        if (this.y < 0){
-            this.y = windowHeight;
-            this.x = random(100, windowWidth - 100);
-        }
-    }
-    
-    show(){
-        textSize(this.size);
-        textStyle(BOLD);
-        textFont(myFont);
-        fill (0);
-        text ("CROWD", this.x, this.y);
-        
-    }
     
     
 }
 
-function SkierFail (){
-    background(0);
+
+function draw() {
+  
+  	let newxtwalkerX = walker.x + walker.speed;
+  	let newxtwalkerY = walker.y + walker.speed;
+    
+      drawSprites();
+
+    
+  
+    capture.loadPixels();
+    if (capture.pixels.length > 0) { // don't forget this!
+        var pixels = capture.pixels;
+        var thresholdAmount = thresh;
+        thresholdAmount /= 100.0; // this is the slider range
+        thresholdAmount *= 255; // this is the maximum value
+        var total = 0;
+        var i = 0;
+        for (var y = 0; y < h; y++) {
+            for (var x = 0; x < w; x++) {
+              	
+              	
+              
+              	// here we check whether pixel should be white (255) 
+                // or black (0)
+                var redValue = pixels[i]; // 120
+                var outputValue = 0;
+              	// if red value is higher than threshhold, pixel should
+                // be white:
+                if (redValue >= thresholdAmount) {
+                    outputValue = 255;
+                    total++;
+                }
+              
+              
+              	if(x == newxtwalkerX && y == newxtwalkerY){
+                  if (outputValue == 0){
+                		walker.speed *= 0;
+                  }else{
+                      walker.speed = 1;
+                  }
+                  
+                }
+              	
+              	
+              
+              
+                // now that we have decided whether white or black
+                // we updated all color channels of this pixel
+                pixels[i++] = outputValue; // set red i == 0, then increases by one
+                pixels[i++] = outputValue; // set green
+                pixels[i++] = outputValue; // set blue
+                i++; // skip alpha                
+            }
+        }
+
+        var n = w * h;
+        var ratio = total / n;
+        // select('#percentWhite').elt.innerText = int(100 * ratio);
+    }
+    capture.updatePixels();
+  	
+  	image(capture, 0, 0, 1280, 960);
+  	
+  	walker.update();
+  	walker.display();
+
     
 }
 
 
+
+class Walker{
+  constructor(){
+    this.x = 10;
+    this.y = 10;
+    this.speed = 1;
+  }
+  update(){
+    this.x += this.speed;
+    this.y += this.speed;
     
+  }
+  display(){
+    fill(255,0,0);
+   	ellipse(this.x, this.y, 10, 10); 
+  }
+  
+}
