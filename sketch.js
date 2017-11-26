89a14cd00a1a6354517b5cd697bfcdf55321d560
Hbentzur <hdr.bentzur@gmail.com>
2017-11-26 14:21:32 -0500

    Walker_class_and_Play_26_11_1421
    I need to chack if it can work only with play.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
-var capture;
-var thresh = 10;
+var camera;
+var thresh = 30;
 
-var w = 640,
-    h = 480;
+var w = 1024,
+    h = 768;
 
-
-var Walk;
+var Noman;
 
 function setup() {
+
     // camera
-    capture = createCapture(VIDEO);
+    camera = createCapture(VIDEO);
     createCanvas(w, h);
-    capture.size(w, h);
-    capture.hide();
+    camera.size(w, h);
+    camera.hide();
 
     // No walking sequence
-    Walk =
+    Noman =
         createSprite(width / 2, height / 2, 60, 60);
-    Walk.addAnimation("Graphic/Walk/Walk_1.png", "Graphic/Walk/Walk_2.png", "Graphic/Walk/Walk_3.png");
-    Walk.rotateToDirection = true;
-    Walk.maxSpeed = 3;
+    Noman.addAnimation("Graphic/Walk/Walk_1.png", "Graphic/Walk/Walk_2.png", "Graphic/Walk/Walk_3.png");
+    Noman.rotateToDirection = true;
+    Noman.maxSpeed = 3;
 
     walker = new No();
 
@@ -27,16 +27,24 @@ function setup() {
 
 function draw() {
 
+    walker.Walk();
+
+    // No
+    var newxtwalkerX = walker.Nox + walker.Nospeed;
+    var newxtwalkerY = walker.Noy + walker.Nospeed;
+
 
     //B&W
-  capture.loadPixels();
-    if (capture.pixels.length > 0) { // don't forget this!
-        var pixels = capture.pixels;
+    camera.loadPixels();
+    if (camera.pixels.length > 0) {
+        var pixels = camera.pixels;
         var thresholdAmount = thresh;
-        thresholdAmount /= 100.0; // this is the slider range
-        thresholdAmount *= 255; // this is the maximum value
+        thresholdAmount /= 100.0;
+        thresholdAmount *= 255;
+
         var total = 0;
         var i = 0;
+
         for (var y = 0; y < h; y++) {
             for (var x = 0; x < w; x++) {
                 var redValue = pixels[i];
@@ -45,25 +53,27 @@ function draw() {
                     outputValue = 255;
                     total++;
                 }
-                pixels[i++] = outputValue; // set red
-                pixels[i++] = outputValue; // set green
-                pixels[i++] = outputValue; // set blue
-                i++; // skip alpha                
-            }
+
+
+            pixels[i++] = outputValue; // R
+            pixels[i++] = outputValue; // G
+            pixels[i++] = outputValue; // B
+            i++; // No alpha               
         }
-
-        var n = w * h;
-        var ratio = total / n;
-        //select('#percentWhite').elt.innerText = int(100 * ratio);
     }
-    capture.updatePixels();
 
-    image(capture, 0, 0, 640, 480);
-    
-        drawSprites();
+    var n = w * h;
+    var ratio = total / n;
+}
 
-    // No
-    walker.Walk();
+camera.updatePixels();
+
+image(camera, 0, 0, 1024, 768);
+
+// Animation
+drawSprites();
+
+
 
 
 }
@@ -74,14 +84,27 @@ class No {
     constractor() {
         this.Nox = 10;
         this.Noy = 10;
-        this.Nospeed = 1;
+        this.Nospeed = 10;
     }
 
     Walk() {
-        if (mouseIsPressed) {
-            Walk.attractionPoint(10, mouseX, mouseY);
-        }
+//        if (mouseIsPressed) {
+//            Noman.attractionPoint(3, mouseX, mouseY);
+//        }
+                this.Nox += this.Nospeed;
+                this.Noy += this.Nospeed;
     }
+
+
+}
+
+class Door {
+    constractor() {
+        this.Doorx = (random(100, width));
+        this.Doory = (random(100, height));
+    }
+
+
 
 
 }
